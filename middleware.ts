85770c70de5446a63383bd3f1b345a92dca0f6e3
Cristian Frutos Solé <cristianfrutossole@MacBook-Pro-de-Cristian.local>
2026-05-06 17:28:59 +0200

    Limitar middleware a rutas privadas

--- middleware.ts
+++ middleware.ts
@@ -38,6 +38,9 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/mi-cuenta/:path*",
+    "/crear-contrasena/:path*",
+    "/admin-clientes/:path*",
+    "/api/customer/:path*",
   ],
 };
